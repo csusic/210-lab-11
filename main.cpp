6da2854f032a1d976b76c0963f69e5c8ef1bbaf1
Christine Susic <csusic022@insite.4cd.edu>
2026-02-21 16:03:54 -0800

    Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// COMSC-210 | Lab 11 | Christine Susic
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 4;
+
+struct Inventory {
+    string name;
+    int partNum;
+    int* quantity = new int[SIZE];
+}
+
+int main() {
+    
+    
+    return 0;
+}
