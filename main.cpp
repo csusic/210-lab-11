9235cdcb2bf9dff97011f5e188e1204f45a1dc81
Christine Susic <csusic022@insite.4cd.edu>
2026-02-21 16:33:50 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -3,39 +3,59 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 4;
+const int INVENTORY_SIZE = 2;
+const int QTY_SIZE = 3;
 
 struct Inventory {
     string name;
     int partNum;
-    int* quantity = new int[SIZE];
+    int* qty = new int[INVENTORY_SIZE];
     
     ~Inventory() {
-        if (quantity) 
-            delete [] quantity;
-        quantity = nullptr;
+        if (qty) 
+            delete [] qty;
+        qty = nullptr;
     }
-}
+};
 
 //function prototypes
 void inputInventory(Inventory *);
 void displayInventory(Inventory *);
 
 int main() {
+    Inventory *qty = new Inventory[INVENTORY_SIZE];
+    
+    for (int i = 0; i < INVENTORY_SIZE; i++)
+        inputInventory(&qty[i]);
+    
+    for (int i = 0; i < INVENTORY_SIZE; i++)
+        displayInventory(&qty[i]);
     
     return 0;
 }
 
 void inputInventory(Inventory * iptr) {
-    int counter = 0; //number counter
-    for (int i = 0; i < SIZE; i++) {
-        cout << "Inventory #" << counter++ << ":";
+    int counter = 1; //number counter
+    for (int i = 0; i < QTY_SIZE; i++) {
+        cout << "Inventory #" << counter++ << ":\n";
         cout << "Name: ";
-        getline(cin, iptr->name)
+        getline(cin, iptr->name);
         cout << "Part Number: ";
         cin >> iptr->partNum;
-        sptr->quantity = new int[SIZE];
-        for 
+        iptr->qty = new int[QTY_SIZE];
         cout << "Quantity: ";
-        cin >> iptr->quantity[i];
+        cin >> iptr->qty[i];
+        cout << endl;
     }
+}
+    
+void displayInventory(Inventory * iptr) {
+    int counter = 1; //number counter
+    for (int i = 0; i < QTY_SIZE; i++) {
+        cout << "Inventory Summary" << endl; 
+        cout << "Inventory #" << counter++ << ":";
+        cout << "Name: " << iptr->name << endl;
+        cout << "Part Number: " << iptr->partNum << endl;
+        cout << "Quantity: " << iptr->qty[i] << endl;
+    }
+}
