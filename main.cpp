4eac0836c805f7abfea56f806eba72648a2b74b1
Christine Susic <csusic022@insite.4cd.edu>
2026-02-21 17:03:53 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 using namespace std;
 
+//const size integers for inventory and color
 const int INVENTORY_SIZE = 3;
 const int COLOR_SIZE = 4;
 
+//inventory struct to hold name, partNum, and color array
 struct Inventory {
     string name;
     int partNum;
     int* color = new int[INVENTORY_SIZE];
     
+    //delete memory and set pointer to null
     ~Inventory() {
         if (color) 
             delete [] color;
@@ -23,6 +26,7 @@ void inputInventory(Inventory *);
 void displayInventory(Inventory *);
 
 int main() {
+    //pointer to inventory struct
     Inventory *data = new Inventory[INVENTORY_SIZE];
     
     //input inventory function
@@ -38,7 +42,7 @@ int main() {
 
 //input inventory function
 void inputInventory(Inventory * iptr) {
-    //output header
+    //header
     cout << "Inventory" << endl;
     //input name
     cout << "Name: ";
@@ -46,8 +50,8 @@ void inputInventory(Inventory * iptr) {
     //input part number
     cout << "Part Number: ";
     cin >> iptr->partNum;
+    //input color array
     iptr->color = new int[COLOR_SIZE];
-    //input color
     for (int i = 0; i < COLOR_SIZE; i++) {
         cout << "Color #" << i + 1 << " : ";
         cin >> iptr->color[i]; 
@@ -58,15 +62,16 @@ void inputInventory(Inventory * iptr) {
 
 //display inventory function
 void displayInventory(Inventory * iptr) {
-    //output header
+    //header
     cout << "Inventory Summary" << endl; 
     //output name
     cout << "Name: " << iptr->name << endl;
     //output part number
     cout << "Part Number: " << iptr->partNum << endl;
-    //output color
+    //output color array
     for (int i = 0; i < COLOR_SIZE; i++) {
         cout << "Color #" << i + 1 << " : ";
         cout << iptr->color[i] << endl;
     }
-}
+    cout << endl;
+}   
