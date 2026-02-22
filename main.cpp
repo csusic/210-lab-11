1eb8f7ecfca8a02ad02be206b282aae3bd763764
Christine Susic <csusic022@insite.4cd.edu>
2026-02-21 16:43:49 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 using namespace std;
 
-const int INVENTORY_SIZE = 2;
-const int QTY_SIZE = 3;
+const int INVENTORY_SIZE = 3;
+const int COLOR_SIZE = 4;
 
 struct Inventory {
     string name;
     int partNum;
-    int* qty = new int[INVENTORY_SIZE];
+    int* color = new int[INVENTORY_SIZE];
     
     ~Inventory() {
-        if (qty) 
-            delete [] qty;
-        qty = nullptr;
+        if (color) 
+            delete [] color;
+        color = nullptr;
     }
 };
 
@@ -23,39 +23,50 @@ void inputInventory(Inventory *);
 void displayInventory(Inventory *);
 
 int main() {
-    Inventory *qty = new Inventory[INVENTORY_SIZE];
+    Inventory *color = new Inventory[INVENTORY_SIZE];
     
     for (int i = 0; i < INVENTORY_SIZE; i++)
-        inputInventory(&qty[i]);
+        inputInventory(&color[i]);
     
     for (int i = 0; i < INVENTORY_SIZE; i++)
-        displayInventory(&qty[i]);
+        displayInventory(&color[i]);
     
     return 0;
 }
 
 void inputInventory(Inventory * iptr) {
     int counter = 1; //number counter
-    for (int i = 0; i < QTY_SIZE; i++) {
-        cout << "Inventory #" << counter++ << ":\n";
-        cout << "Name: ";
-        getline(cin, iptr->name);
-        cout << "Part Number: ";
-        cin >> iptr->partNum;
-        iptr->qty = new int[QTY_SIZE];
-        cout << "Quantity: ";
-        cin >> iptr->qty[i];
-        cout << endl;
+    //output header
+    cout << "Inventory #" << counter++ << ":\n";
+    //input name
+    cout << "Name: ";
+    getline(cin, iptr->name);
+    //input part number
+    cout << "Part Number: ";
+    cin >> iptr->partNum;
+    iptr->color = new int[COLOR_SIZE];
+    //input color
+    for (int i = 0; i < COLOR_SIZE; i++) {
+        cout << "Color #: " << counter++;
+        cin >> iptr->color[i]; 
     }
+    cout << endl;
+    cin.ignore();
 }
-    
+
+//display inventory function
 void displayInventory(Inventory * iptr) {
     int counter = 1; //number counter
-    for (int i = 0; i < QTY_SIZE; i++) {
-        cout << "Inventory Summary" << endl; 
-        cout << "Inventory #" << counter++ << ":";
-        cout << "Name: " << iptr->name << endl;
-        cout << "Part Number: " << iptr->partNum << endl;
-        cout << "Quantity: " << iptr->qty[i] << endl;
+    //output headers
+    cout << "Inventory Summary" << endl; 
+    cout << "Inventory #" << counter++ << ":";
+    //output name
+    cout << "Name: " << iptr->name << endl;
+    //output part number
+    cout << "Part Number: " << iptr->partNum << endl;
+    //output color
+    for (int i = 0; i < COLOR_SIZE; i++) {
+        cout << "Color #: " << counter++;
+        cout << iptr->color[i] << endl;
     }
 }
